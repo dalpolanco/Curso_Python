fbfe33af7d699795097a2e6006863d4608009f50
Daniel Polanco <dalpolanco@gmail.com>
2026-04-13 16:54:19 -0400

    archivo 03

---
+++ Tipos/03-format-strings.py
@@ -0,0 +1,15 @@
+nombre = "Daniel"
+apellido = "Polanco"
+# nombre_completo = nombre + " " + apellido
+# print(nombre_completo)
+
+nombre_completo = f"{nombre} {apellido}"
+print(nombre_completo)
+
+
+nombre_completo = f"{nombre[0]} {apellido[1]}"
+print(nombre_completo)
+
+
+nombre_completo = f"{nombre[0]} {2 + 7}"
+print(nombre_completo)
