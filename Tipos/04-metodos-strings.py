6b3662e63fbdd6a5bad55386a3d629b857285e51
Daniel Polanco <dalpolanco@gmail.com>
2026-04-13 17:40:01 -0400

    archivo 04 terminado

---
+++ Tipos/04-metodos-strings.py
@@ -0,0 +1,13 @@
+animal = "  chanCHito feLiz  "
+print(animal.upper())
+print(animal.lower())
+print(animal.strip().capitalize())
+print(animal.title())
+print(animal.strip())
+print(animal.lstrip())
+print(animal.rstrip())
+print(animal.find("CH"))
+print(animal.find("AW"))
+print(animal.replace("nCH", "j"))
+print("nCH" in animal)
+print("nCH" not in animal)
