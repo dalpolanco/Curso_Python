45b070a0b4c6d2068e9c7b1f7c9637dfd0907700
Daniel Polanco <dalpolanco@gmail.com>
2026-04-13 16:25:50 -0400

    nuevo archivo

---
+++ Tipos/01-variables.py
@@ -0,0 +1,9 @@
+nombre_curso = "Ultimate Python"
+nombre_curso2 = "Ultimate Python 2"
+nombre_curso3 = "Ultimate Python 3"
+nombre_curso4 = "Ultimate Python 4"
+print(nombre_curso, nombre_curso2, nombre_curso3, nombre_curso4)
+alumnos = 5000
+puntaje = 9.9
+publicado = True
+print(alumnos, puntaje, publicado)
