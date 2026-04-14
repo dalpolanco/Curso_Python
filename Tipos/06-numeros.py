c2dbfe810ab8738cac9131cf827d5b1a1154a620
Daniel Polanco <dalpolanco@gmail.com>
2026-04-14 13:07:05 -0400

    manejo de numeros

---
+++ Tipos/06-numeros.py
@@ -0,0 +1,16 @@
+numero = 2  # integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # 2 + 2i
+
+# numero = numero + 2
+# numero += 3
+numero *= 5
+
+print("Valor de variable numero:", numero)
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(4 / 2)
+print(1 // 3)
+print(8 % 3)
+print(2 ** 3)
