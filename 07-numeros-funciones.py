6d99255c15d190d9fa19f1d92d76cd763911953f
Daniel Polanco <dalpolanco@gmail.com>
2026-04-27 14:39:34 -0400

    actualizacion 07-numeros funciones

--- 07-numeros-funciones.py
+++ 07-numeros-funciones.py
@@ -1,10 +1,12 @@
 import math
 
+print(round(1.3))
 print(round(1.7))
-
+print(round(1.5))
 print(abs(-77))
 print(abs(55))
 
-print("Función Ceil:", math.ceil(1.1))
 
+print("Función Ceil:", math.ceil(1.1))
 print("Función Floor:", math.floor(1.999999999))
+# print("Funcion isnan:", math.isnan("23"))
